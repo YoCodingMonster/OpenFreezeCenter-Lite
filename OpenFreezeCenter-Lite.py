c895ec7cf5b129f7bc0bd90ab1a10787b56ce51d
Aditya Kumar Bajpai <77596578+YoCodingMonster@users.noreply.github.com>
2023-09-22 03:04:07 +0530

    Update OpenFreezeCenter-Lite.py

--- OpenFreezeCenter-Lite.py
+++ OpenFreezeCenter-Lite.py
@@ -1,124 +1,112 @@
 #! /usr/bin/python3
 
 import os
-import ECTweaker as ECT
+import ECTweaker_test as ECT
 
-AUTO_SPEED = [0,40,48,56,64,72,80,0,48,56,64,72,79,86]
-# AUTO_SPEED_CREATED HERE
+# CPU_FAN_PROFILE_BYTE                  VALUES[0][0]   BYTE ADDRESS              FAN PROFILES BYTE AND VALUES
+# CPU_FAN_PROFILE_VALUE_AUTO            VALUES[0][1]   VALUE
+# CPU_FAN_PROFILE_VALUE_ADVANCED        VALUES[0][2]   VALUE
 
-# CPU_FAN_PROFILE_BYTE             VALUES[0][0]   BYTE ADDRESS              FAN PROFILES BYTE AND VALUES
-# CPU_FAN_PROFILE_VALUE_AUTO       VALUES[0][1]   BYTE VALUE
-# CPU_FAN_PROFILE_VALUE_ADVANCED   VALUES[0][2]   BYTE VALUE
+# CPU_COOLER_BOOSTER_BYTE               VALUES[1][0]   BYTE ADDRESS              COOLER BOOSTER BYTE AND VALUES
+# CPU_COOLER_BOOSTER_VALUE              VALUES[1][1]   VALUE [off]
+# CPU_COOLER_BOOSTER_VALUE              VALUES[1][2]   VALUE [on]
 
-# CPU_COOLER_BOOSTER_BYTE          VALUES[1][0]   BYTE ADDRESS              COOLER BOOSTER BYTE AND VALUES
-# CPU_COOLER_BOOSTER_VALUE         VALUES[1][1]   BYTE VALUE [off]
-# CPU_COOLER_BOOSTER_VALUE         VALUES[1][2]   BYTE VALUE [on]
+# CPU_FAN_SPEED_1_BYTE                  VALUES[2][0]   BYTE ADDRESS              CPU FAN SPEEDS ADDRESS
+# CPU_FAN_SPEED_2_BYTE                  VALUES[2][1]   BYTE ADDRESS
+# CPU_FAN_SPEED_3_BYTE                  VALUES[2][2]   BYTE ADDRESS
+# CPU_FAN_SPEED_4_BYTE                  VALUES[2][3]   BYTE ADDRESS
+# CPU_FAN_SPEED_5_BYTE                  VALUES[2][4]   BYTE ADDRESS
+# CPU_FAN_SPEED_6_BYTE                  VALUES[2][5]   BYTE ADDRESS
+# CPU_FAN_SPEED_7_BYTE                  VALUES[2][6]   BYTE ADDRESS
 
-# CPU_FAN_SPEED_1_BYTE             VALUES[2][0]   BYTE ADDRESS              CPU FAN SPEEDS ADDRESS
-# CPU_FAN_SPEED_2_BYTE             VALUES[2][1]   BYTE ADDRESS
-# CPU_FAN_SPEED_3_BYTE             VALUES[2][2]   BYTE ADDRESS
-# CPU_FAN_SPEED_4_BYTE             VALUES[2][3]   BYTE ADDRESS
-# CPU_FAN_SPEED_5_BYTE             VALUES[2][4]   BYTE ADDRESS
-# CPU_FAN_SPEED_6_BYTE             VALUES[2][5]   BYTE ADDRESS
-# CPU_FAN_SPEED_7_BYTE             VALUES[2][6]   BYTE ADDRESS
+# GPU_FAN_SPEED_1_BYTE                  VALUES[3][0]   BYTE ADDRESS              GPU FAN SPEEDS ADDRESS
+# GPU_FAN_SPEED_2_BYTE                  VALUES[3][1]   BYTE ADDRESS
+# GPU_FAN_SPEED_3_BYTE                  VALUES[3][2]   BYTE ADDRESS
+# GPU_FAN_SPEED_4_BYTE                  VALUES[3][3]   BYTE ADDRESS
+# GPU_FAN_SPEED_5_BYTE                  VALUES[3][4]   BYTE ADDRESS
+# GPU_FAN_SPEED_6_BYTE                  VALUES[3][5]   BYTE ADDRESS
+# GPU_FAN_SPEED_7_BYTE                  VALUES[3][6]   BYTE ADDRESS
 
-# GPU_FAN_SPEED_1_BYTE             VALUES[3][0]   BYTE ADDRESS              GPU FAN SPEEDS ADDRESS
-# GPU_FAN_SPEED_2_BYTE             VALUES[3][1]   BYTE ADDRESS
-# GPU_FAN_SPEED_3_BYTE             VALUES[3][2]   BYTE ADDRESS
-# GPU_FAN_SPEED_4_BYTE             VALUES[3][3]   BYTE ADDRESS
-# GPU_FAN_SPEED_5_BYTE             VALUES[3][4]   BYTE ADDRESS
-# GPU_FAN_SPEED_6_BYTE             VALUES[3][5]   BYTE ADDRESS
-# GPU_FAN_SPEED_7_BYTE             VALUES[3][6]   BYTE ADDRESS
+# AUTO_FAN_SPEEDS_VENDOR_VALUES         VALUES[4][]    BYTE ADDRESS              AUTO FAN SPEEDS
 
-# AUTO_FAN_SPEEDS_VENDOR_VALUES    VALUES[4][]    BYTE ADDRESS              AUTO FAN SPEEDS
+# ADVANCED_FAN_SPEEDS_VENDOR_VALUES     VALUES[5][]    BYTE ADDRESS              ADVANCED FAN SPEEDS
 
-# CPU_CURRENT_TEMPERATURE_BYTE     VALUES[5][0]   BYTE ADDRESS              CPU CURRENT TEMPERATURE ADDRESS
-
+AUTO_SPEED = []
 VALUES = []
 
 # Select the FAN CURVE profiles
 
-def cpu_gen():
-    CPU_GEN = input("is your CPU Intel 10th gen and above [y/n] :-> ")
-    if CPU_GEN == 'y' or CPU_GEN == 'Y':
-        CPU_GEN = "NEW"
-    elif CPU_GEN == 'n' or CPU_GEN == 'N':
-        CPU_GEN = "OLD"
-    else:
-        cpu_gen()
-    
-    if CPU_GEN == "NEW":
-        VALUES = [[0xd4, 13, 141],
-                  [0x98, 2, 130],
-                  [0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78],
-                  [0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90],
-                  AUTO_SPEED]
-    else:
-        VALUES = [[0xf4, 12, 140],
-                  [0x98, 0, 128],
-                  [0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78],
-                  [0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90],
-                  AUTO_SPEED]
-    return VALUES
-
 def set_fan_mode():
-    VALUES = cpu_gen()
-    MODE = int(input("Set Fan Mode\n1 - Auto\n2 - Basic\n3 - Advanced\n4 - Cooler Booster [1 or 2 or 3 or 4] :-> "))
+    MODE = int(input("Set Fan Mode\n1 - Auto\n2 - Advanced\n3 - Cooler Booster [1 or 2 or 3] :-> "))
 
-    if MODE == 1:
-        ECT.fan_profile("auto", VALUES)
+    if MODE == 1: ECT.fan_profile("auto", VALUES)
 
-    elif MODE == 2:
-        OFFSET = int(input("Select one option for fan speed\n1 - Slower\n2 - Slow\n3 - Normal\n4 - Fast\n5 - Faster [1 or 2 or 3 or 4 or 5] :-> "))
-        while OFFSET > 0 and OFFSET < 6:
-            for i in range(len(AUTO_SPEED)):
-                AUTO_SPEED[i] += (OFFSET * 10) - 30
-                if AUTO_SPEED[i] < 0:
-                    AUTO_SPEED[i] = 0
-                if AUTO_SPEED[i] > 150:
-                    AUTO_SPEED[i] = 150
-            ECT.fan_profile("advanced", VALUES)
-            break
-        else:
-            OFFSET = int(input("Select one option for fan speed\n1 - Slower\n2 - Slow\n3 - Normal\n4 - Fast\n5 - Faster [1 or 2 or 3 or 4 or 5] :-> "))
+#    elif MODE == 2 and CPU == 0:
+#        OFFSET = int(input("Select one option for fan speed\n1 - Slower\n2 - Slow\n3 - Normal\n4 - Fast\n5 - Faster [1 or 2 or 3 or 4 or 5] :-> "))
+#        while OFFSET < 1 and OFFSET > 5:
+#            OFFSET = int(input("Select one option for fan speed\n1 - Slower\n2 - Slow\n3 - Normal\n4 - Fast\n5 - Faster [1 or 2 or 3 or 4 or 5] :-> "))
+#        BASIC_SPEED = AUTO_SPEED
+#        for i in range(len(AUTO_SPEED)):
+#            BASIC_SPEED[i] += (OFFSET * 10) - 30
+#            if BASIC_SPEED[i] < 0:
+#                BASIC_SPEED[i] = 0
+#            if BASIC_SPEED[i] > 150:
+#                BASIC_SPEED[i] = 150
+#        VALUES[4] = BASIC_SPEED
+#        ECT.fan_profile("basic", VALUES, CPU)
+    
+    elif MODE == 2: ECT.fan_profile("advanced", VALUES)
 
     elif MODE == 3:
-        ADV_SPEED_CPU = [0, 50, 75, 100, 125, 150, 150] # CPU FAN speed at LOWEST, LOWER, LOW, MEDIUM, HIGH, HIGHER, HIGHEST CPU TEMP
-        ADV_SPEED_GPU = [0, 50, 75, 100, 125, 150, 150] # GPU FAN speed at LOWEST, LOWER, LOW, MEDIUM, HIGH, HIGHER, HIGHEST GPU TEMP
-        VALUES[4] = ADV_SPEED_CPU + ADV_SPEED_GPU
-        ECT.fan_profile("advanced", VALUES)
-
-    elif MODE == 4:
-
         ECT.fan_profile("cooler booster", VALUES)
         print("Cooler Booster - ", "ON" if ECT.read(VALUES[1][0], 1) == VALUES[1][2] else "OFF")
+        if ECT.read(VALUES[1][0], 1) == VALUES[1][1]:
+            if ECT.read(VALUES[0][0], 1) == VALUES[0][1]:
+                print("Back to :-> auto")
+                ECT.fan_profile("auto", VALUES)
+            if ECT.read(VALUES[0][0], 1) == VALUES[0][2]:
+                print("Back to :-> advanced")
+                ECT.fan_profile("advanced", VALUES)
 
     else:
         set_fan_mode()
 
-# Function called to set the AUTO fan curve used by the vendor.
+# Function called to set the CONFIG FILE
 
-if 'AUTO_SPEED' not in globals():
-    AUTO_SPEED_CPU = [ECT.read(VALUES[2][0], 1), ECT.read(VALUES[2][1], 1), ECT.read(VALUES[2][2], 1), ECT.read(VALUES[2][3], 1), ECT.read(VALUES[2][4], 1), ECT.read(VALUES[2][5], 1), ECT.read(VALUES[2][6], 1)] # CPU FAN speed at LOWEST, LOWER, LOW, MEDIUM, HIGH, HIGHER, HIGHEST CPU TEMP
-    AUTO_SPEED_GPU = [ECT.read(VALUES[3][0], 1), ECT.read(VALUES[3][1], 1), ECT.read(VALUES[3][2], 1), ECT.read(VALUES[3][3], 1), ECT.read(VALUES[3][4], 1), ECT.read(VALUES[3][5], 1), ECT.read(VALUES[3][0], 1)] # GPU FAN speed at LOWEST, LOWER, LOW, MEDIUM, HIGH, HIGHER, HIGHEST GPU TEMP
-    AUTO_SPEED = AUTO_SPEED_CPU + AUTO_SPEED_GPU
-
-    with open(__file__, 'r') as file:
-        SCRIPT = file.read().split('\n')
-
-    with open(__file__,'w') as file:
-        NEW_SCRIPT = []
-        for LINE in SCRIPT:
-            if LINE == "# AUTO SPEEDS HERE":
-                LINE = "AUTO_SPEED = ["+str(AUTO_SPEED[0])+","+str(AUTO_SPEED[1])+","+str(AUTO_SPEED[2])+","+str(AUTO_SPEED[3])+","+str(AUTO_SPEED[4])+","+str(AUTO_SPEED[5])+","+str(AUTO_SPEED[6])+","+str(AUTO_SPEED[7])+","+str(AUTO_SPEED[8])+","+str(AUTO_SPEED[9])+","+str(AUTO_SPEED[10])+","+str(AUTO_SPEED[11])+","+str(AUTO_SPEED[12])+","+str(AUTO_SPEED[13])+"]"
-                NEW_SCRIPT.append(LINE)
-            else:
-                NEW_SCRIPT.append(LINE)
-        file.write('\n'.join(NEW_SCRIPT))
+def global_var_check(CHOICE, LINE_YES, LINE_NO):
+    INPUT = input(CHOICE)
+    if INPUT == 'y' or INPUT == 'Y': LINE = LINE_YES
+    elif INPUT == 'n' or INPUT == 'N': LINE = LINE_NO
+    else: global_var_check(CHOICE, LINE_YES, LINE_NO)
+    return LINE
 
 CHECK = ECT.check()
 if CHECK != 1:
-    set_fan_mode()
+    try:
+        CONFIG_FILE = open("config.py", "r")
+    except FileNotFoundError:
+        CONFIG = []
+        CHOICE = "Select [Y/y] if you want universal auto fan profile without booting into windows\nor\nSelect [N/n] if you want to fetch vendor specified auto fan profile which will require you to \n\tPRIMARY. Close this terminal (Before closing read all the steps)\n\t1. boot into windows\n\t2. set the fan profile to auto\n\t3. boot back to linux and then choose this option here! :-> "
+        LINE_YES = "AUTO_SPEED = [0, 40, 48, 56, 64, 72, 80, 0, 48, 56, 64, 72, 79, 86]"
+        AUTO_SPEED_CPU = [ECT.read(0x72, 1), ECT.read(0x73, 1), ECT.read(0x74, 1), ECT.read(0x75, 1), ECT.read(0x76, 1), ECT.read(0x77, 1), ECT.read(0x78, 1)] # CPU FAN speed at LOWEST, LOWER, LOW, MEDIUM, HIGH, HIGHER, HIGHEST CPU TEMP
+        AUTO_SPEED_GPU = [ECT.read(0x8a, 1), ECT.read(0x8b, 1), ECT.read(0x8c, 1), ECT.read(0x8d, 1), ECT.read(0x8e, 1), ECT.read(0x8f, 1), ECT.read(0x90, 1)] # GPU FAN speed at LOWEST, LOWER, LOW, MEDIUM, HIGH, HIGHER, HIGHEST GPU TEMP
+        AUTO_SPEED = AUTO_SPEED_CPU + AUTO_SPEED_GPU
+        LINE_NO = "AUTO_SPEED = ["+str(AUTO_SPEED[0])+", "+str(AUTO_SPEED[1])+", "+str(AUTO_SPEED[2])+", "+str(AUTO_SPEED[3])+", "+str(AUTO_SPEED[4])+", "+str(AUTO_SPEED[5])+", "+str(AUTO_SPEED[6])+", "+str(AUTO_SPEED[7])+", "+str(AUTO_SPEED[8])+", "+str(AUTO_SPEED[9])+", "+str(AUTO_SPEED[10])+", "+str(AUTO_SPEED[11])+", "+str(AUTO_SPEED[12])+", "+str(AUTO_SPEED[13])+"]"
+        CONFIG.append(global_var_check(CHOICE, LINE_YES, LINE_NO))
+
+        CHOICE = "Is your CPU intel 10th Gen and above [Y/N] :-> "
+        LINE_YES = "\nCPU = 1\nVALUES = [[0xd4, 13, 141],\n[0x98, 2, 130],\n[0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78],\n[0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90],\nAUTO_SPEED,\n[0, 50, 75, 100, 125, 150, 150, 0, 50, 75, 100, 125, 150, 150]]\t\t# Edit this list for ADVANCED FAN SPEEDS with  refrence [CPU1, CPU2, CPU3, CPU4, CPU5, CPU6, CPU7, GPU1, GPU2, GPU3, GPU4, GPU5, GPU6, GPU7]"
+        LINE_NO  = "\nCPU = 0\nVALUES = [[0xf4, 12, 140],\n[0x98, 0, 128],\n[0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78],\n[0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90],\nAUTO_SPEED,\n[0, 50, 75, 100, 125, 150, 150, 0, 50, 75, 100, 125, 150, 150]]\t\t# Edit this list for ADVANCED FAN SPEEDS with  refrence [CPU1, CPU2, CPU3, CPU4, CPU5, CPU6, CPU7, GPU1, GPU2, GPU3, GPU4, GPU5, GPU6, GPU7]"
+        CONFIG.append(global_var_check(CHOICE, LINE_YES, LINE_NO))
+
+        CONFIG_FILE = open("config.py", "w")
+        CONFIG_FILE.writelines(CONFIG)
+        CONFIG_FILE.close()
+    finally:
+        import config
+        AUTO_SPEED = config.AUTO_SPEED
+        VALUES = config.VALUES
+        set_fan_mode()
 else:
     os.system("shutdown -r +1")
     print("Rebooting system within 1 min!\nPlease save all work before it happens!")
